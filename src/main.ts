035cd79e82368c092ffb4f1569c9256bfb1d02aa
kvoid <kianheitkamp@outlook.com>
2023-05-10 15:19:41 +0200

    front-end changes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,32 @@
-import { createApp } from "vue";
-import "./styles.css";
-import App from "./App.vue";
+import { invoke } from "@tauri-apps/api/tauri";
 
-createApp(App).mount("#app");
+let greetInputEl: HTMLInputElement | null;
+let greetMsgEl: HTMLElement | null;
+let searchBarEl: HTMLInputElement | null;
+
+async function greet() {
+  if (greetMsgEl && greetInputEl) {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsgEl.textContent = await invoke("greet", {
+      name: greetInputEl.value,
+    });
+  }
+}
+
+async function search() {
+    if (searchBarEl && greetMsgEl) {
+        greetMsgEl.textContent = await invoke("greet", {
+            name: searchBarEl.value,
+          });
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  searchBarEl = document.querySelector("#search-bar")
+
+  document
+    .querySelector("#search-button")
+    ?.addEventListener("click", () => search());
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue()],
-
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent vite from obscuring rust errors
   clearScreen: false,
